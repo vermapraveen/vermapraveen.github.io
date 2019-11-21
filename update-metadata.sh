9a8e1d0ce562106b15bd823036414c39db049b6b
Praveen Verma <verma.praveen@hotmail.com>
2019-11-21 07:20:22 -0600

    adding github environment vriables

--- update-metadata.sh
+++ update-metadata.sh
@@ -9,15 +9,23 @@ for blogfile in $FILES/*; do
   echo $blogfile
 done  | jq -R -s -c 'split("\n")'
 
-
-BUCKET_NAME=testbucket
-OBJECT_NAME=testworkflow-2.0.1.jar
-TARGET_LOCATION=/opt/test/testworkflow-2.0.1.jar
+POST_METADATA_FILE=content/posts.json
+BUCKET_NAME="first part"
+OBJECT_NAME="first-part"
+TARGET_LOCATION="[ "Fiesta", "Focus", "Mustang" ]"
 
 JSON_STRING=$( jq -n \
                   --arg bn "$BUCKET_NAME" \
                   --arg on "$OBJECT_NAME" \
                   --arg tl "$TARGET_LOCATION" \
-                  '{bucketname: $bn, objectname: $on, targetlocation: $tl}' )
+                  '{title: $bn, path: $on, tags: $tl}' )
 
-echo $JSON_STRING
+echo $JSON_STRING > $POST_METADATA_FILE
+
+remote=$(git config --get remote.origin.url)
+repo=$(basename $remote .git)
+
+echo $remote
+echo $repo
+# git add $POST_METADATA_FILE
+# git commit -m "metadata-file-updated #patch"
