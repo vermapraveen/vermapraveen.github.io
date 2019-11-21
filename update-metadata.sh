e519fcf522f3d3ea76d23afeea6130caa7eba827
Praveen Verma <verma.praveen@hotmail.com>
2019-11-21 06:34:07 -0600

    trying executing json creator while build

---
+++ update-metadata.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+set -e
+
+FILES="content/posts"
+
+for blogfile in $FILES/*; do
+  echo $blogfile
+done  < jq -R -s -c 'split("\n")'
+
+
+BUCKET_NAME=testbucket
+OBJECT_NAME=testworkflow-2.0.1.jar
+TARGET_LOCATION=/opt/test/testworkflow-2.0.1.jar
+
+JSON_STRING=$( jq -n \
+                  --arg bn "$BUCKET_NAME" \
+                  --arg on "$OBJECT_NAME" \
+                  --arg tl "$TARGET_LOCATION" \
+                  '{bucketname: $bn, objectname: $on, targetlocation: $tl}' )
+
+echo $JSON_STRING
