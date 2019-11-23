3ddc9a84f85abb661fea3126c0f3bacde6754201
auto-github-action <verma.praveen@gmail.com>
2019-11-23 07:04:03 -0600

    printing build step

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ POST_METADATA_FILE=content/metadata.json
 
 postJson='['
 for blogfile in $FILES/*; do
+    echo "working on file: $blogfile"
     str=$(awk '/^---/ {mark++; next} mark == 1 {print}' $blogfile)
     postJson=$postJson'{"path":"'$blogfile'" ,'$str'}'
 done
@@ -14,6 +15,8 @@ postJson=$postJson']'
 REPLACE='}{'
 WITH='},{'
 postJson="${postJson//$REPLACE/$WITH}" 
+echo "updating file: $POST_METADATA_FILE"
 echo $postJson > $POST_METADATA_FILE
+cat $POST_METADATA_FILE
 
 
