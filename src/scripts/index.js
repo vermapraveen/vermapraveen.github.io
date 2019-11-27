72b2bd2205b3685d24e67c9937c8a298a1b08a8b
auto-github-action <verma.praveen@gmail.com>
2019-11-27 07:06:41 -0600

    updated style. Kind of OK

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,70 +1,95 @@
-window.onload=function(){
+window.onload = function () {
     var markdownFileName = window.location.search.substring(4);
-    document.getElementById('editOnGithub').setAttribute("href","https://github.com/vermapraveen/vermapraveen.github.io/blob/master/"+markdownFileName)
+    document.getElementById('editOnGithub').setAttribute("href", "https://github.com/vermapraveen/vermapraveen.github.io/blob/master/" + markdownFileName)
 
     getPostData(markdownFileName);
 }
 
 function getPostData(markdownFileName) {
-    fetch('/' +markdownFileName)
+    fetch('/' + markdownFileName)
         .then(contentResponse => contentResponse.text())
         .then(contentText => {
             var showdn = new Showdown.converter();
             var elementToDisplay = document.getElementById('blog-container');
             var mdText = showdn.makeHtml(unescapeHTML(contentText));
-            //applyFirstStyle(mdText);										
-            var divToAdd = document.createElement("DIV");
-            divToAdd.innerHTML = mdText;
+            //applyFirstStyle(mdText);            
+            
+            var blogContentDiv = document.createElement("DIV");
+            blogContentDiv.setAttribute('class', 'blog-content');
 
-            var metadataElement = divToAdd.getElementsByTagName('p')[0];
-            var hrBefore = divToAdd.getElementsByTagName('hr')[0];
-            var hrAfter = divToAdd.getElementsByTagName('hr')[1];
+            var blogMarkupDiv = document.createElement("DIV");
+            blogMarkupDiv.setAttribute('class', 'blog-markup');
+
+            blogMarkupDiv.innerHTML = mdText;
+
+            var metadataElement = blogMarkupDiv.getElementsByTagName('p')[0];
+            blogMarkupDiv.removeChild(metadataElement);
+
+            var hrBefore = blogMarkupDiv.getElementsByTagName('hr')[0];
+            var hrAfter = blogMarkupDiv.getElementsByTagName('hr')[1];
             hrBefore.parentNode.removeChild(hrBefore);
             hrAfter.parentNode.removeChild(hrAfter);
 
-            var jsontext='{'+metadataElement.innerText+'}';
+            blogContentDiv.appendChild(blogMarkupDiv);
+
+            var matadataDiv = document.createElement("DIV");
+            matadataDiv.setAttribute('class', 'blogItemInfoContainer');
+
+            var jsontext = '{' + metadataElement.innerText + '}';
             var postMeta = JSON.parse(jsontext);
 
             if (postMeta) {
-                if (postMeta.date) {
-                    var dateDivToAdd = document.createElement("DIV");
-                    dateDivToAdd.setAttribute("id", "blogCreationDate");
-                    dateDivToAdd.innerHTML = postMeta.date;
-                    divToAdd.appendChild(dateDivToAdd);
-                }
 
-                if (postMeta.title) {
-                    var titleDivToAdd = document.createElement("DIV");
-                    titleDivToAdd.setAttribute("id", "blogTitle");
-                    titleDivToAdd.innerHTML = postMeta.title;
-                    divToAdd.appendChild(titleDivToAdd);
-                }
+                // var row1 = document.createElement("DIV");
+                // row1.setAttribute("class", "row");
 
-                if (postMeta.thumbnail) {
-                    var thumbnailDivToAdd = document.createElement("IMG");
-                    thumbnailDivToAdd.setAttribute("id", "blogThumbnail");
-                    thumbnailDivToAdd.setAttribute("src", '/' + postMeta.thumbnail);
 
-                    divToAdd.appendChild(thumbnailDivToAdd);
-                }
+                var thumbnailContainer = document.createElement("DIV");
+                thumbnailContainer.classList.add("blogItemImgContainer");
 
-                if (postMeta.tags) {
-                    var tagListToAdd = document.createElement("UL");
-                    tagListToAdd.setAttribute("id", "blogTags");
+                var thumbnailDivToAdd = document.createElement("IMG");
+                thumbnailDivToAdd.setAttribute("class", "blogThumbnail");
+                thumbnailDivToAdd.setAttribute("src", '/' + postMeta.thumbnail);
 
-                    postMeta.tags.forEach(function (aTagData) {
-                        var tagToAdd = document.createElement("LI");
-                        var tagText = document.createTextNode(aTagData);
-                        tagToAdd.appendChild(tagText);
-                        tagListToAdd.appendChild(tagToAdd);
-                    });
+                thumbnailContainer.appendChild(thumbnailDivToAdd);
+                matadataDiv.appendChild(thumbnailContainer);
 
-                    divToAdd.appendChild(tagListToAdd);
-                }
+
+                var blogItemTxtContainer = document.createElement("DIV");
+                blogItemTxtContainer.classList.add("blogItemTxtContainer");
+
+                var blogItemTxt = document.createElement("h2");
+                var textNode = document.createTextNode(postMeta.title);
+                blogItemTxt.appendChild(textNode);
+                blogItemTxt.setAttribute('class', 'blogItemTxt');
+
+                var blogItemDateContainer = document.createElement("DIV");
+                blogItemDateContainer.setAttribute('class', 'blogItemDateContainer');
+                var textDateNode = document.createTextNode(postMeta.date);
+                blogItemDateContainer.appendChild(textDateNode);
+
+                blogItemTxtContainer.appendChild(blogItemTxt);
+                blogItemTxtContainer.appendChild(blogItemDateContainer);
+
+                var tagListToAdd = document.createElement("DIV");
+                tagListToAdd.classList.add("blog-tag-list");
+
+                postMeta.tags.forEach(function (aTagData) {
+                    var tagToAdd = document.createElement("DIV");
+                    tagToAdd.setAttribute("class", "blog-tag");
+
+                    var tagText = document.createTextNode(aTagData);
+                    tagToAdd.appendChild(tagText);
+                    tagListToAdd.appendChild(tagToAdd);
+                });
+
+                blogItemTxtContainer.appendChild(tagListToAdd);
+
+                matadataDiv.appendChild(blogItemTxtContainer);
             }
 
-            metadataElement.style.display = 'none';
-            elementToDisplay.appendChild(divToAdd);
+            elementToDisplay.appendChild(matadataDiv);
+            elementToDisplay.appendChild(blogContentDiv);
         });
 };
 
