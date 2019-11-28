0268141102a6db1b2a312150a7ef0371e750ef75
auto-github-action <verma.praveen@gmail.com>
2019-11-28 05:59:21 -0600

    list container to show in middle of page

--- src/scripts/metadata.js
+++ src/scripts/metadata.js
@@ -61,7 +61,16 @@ function getBasicData() {
         blogItemInfoContainer.appendChild(blogItemImgContainer);
         blogItemInfoContainer.appendChild(blogItemTxtContainer);
 
-        menuLink.appendChild(blogItemInfoContainer);
+        var blogContainer_1 = document.createElement("DIV");
+        blogContainer_1.setAttribute('class', 'blog-info-container-1');
+
+        var blogContainer_2 = document.createElement("DIV");
+        blogContainer_2.setAttribute('class', 'blog-info-container-2');
+
+        blogContainer_1.appendChild(blogContainer_2);
+        blogContainer_2.appendChild(blogItemInfoContainer);
+
+        menuLink.appendChild(blogContainer_1);
 
         return menuLink
 
